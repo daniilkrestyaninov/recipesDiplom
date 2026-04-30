43d25106ff076b26db2aafc6154b268fc8b2105e
daniiilkrestyaninov <krestaninovdaniil@yandex.ru>
2026-04-30 21:32:57 +0400

    CORS

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'recipes_db',
+  process.env.DB_NAME || 'diplom',
   process.env.DB_USER || 'postgres',
-  process.env.DB_PASS || 'password',
+  process.env.DB_PASS || 'root',
   {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
